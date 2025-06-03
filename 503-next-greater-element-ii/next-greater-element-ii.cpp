4189ba324ddf40446bc4da23b1512ffc3a3d2744
Neerath0804 <139732091+Neerath0804@users.noreply.github.com>
2025-06-03 11:05:57 +0530

    Time: 7 ms (38.78%) | Memory: 28.4 MB (63.03%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nge(n, -1);
+        stack<int> st;  // store indices
+
+        for(int i = 2 * n - 1; i >= 0; i--) {
+            int currIndex = i % n;
+
+            while (!st.empty() && nums[st.top()] <= nums[currIndex]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                nge[currIndex] = st.empty() ? -1 : nums[st.top()];
+            }
+
+            st.push(currIndex);
+        }
+
+        return nge;
+    }
+};
