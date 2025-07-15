5b9fb3c84631fac9e2d583a8c0cb6a96b30533df
Neerath0804 <139732091+Neerath0804@users.noreply.github.com>
2025-07-15 10:46:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (77.13%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n <= 0)return false;
+        long long power = 1;
          for(int i = 0; i < 50; i++)
      {
-        if(pow(3,i) == n)return true;
-        else if(pow(3,i) > n)return false;
+        if(power == n)return true;
+        else if(power > n)return false;
+        power *= 3;
      }
         return false;
     }
