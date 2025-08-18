df21a83530c731ad6499e37f6c8fadb9f024228c
Neerath0804 <139732091+Neerath0804@users.noreply.github.com>
2025-08-18 09:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,24 +3,24 @@ public:
     bool isValid(string s) {
         int n = s.length();
         stack<char>st;
-        for(int i = 0;i<n;i++)
+        for(int i = 0; i < n; i++)
         {
-            if(s[i] == '('|| s[i] == '{'|| s[i] == '[')
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
             {
                 st.push(s[i]);
             }
-            else if(s[i] == ')' || s[i] == '}' || s[i] == ']')
+            else 
             {
-                if (st.empty()||(s[i] == ')' && st.top() != '(') ||
-                (s[i] == '}' && st.top() != '{') ||
-                (s[i] == ']' && st.top() != '['))
-
-                 {
-                    return false;
-                 }
+                if(st.size() == 0)return false;
+                if((st.top() == '(' && s[i] == ')') 
+                ||(st.top() == '[' &&  s[i] == ']') 
+                || (st.top() == '{' && s[i] == '}'))
+            {
                 st.pop();
             }
+            else return false;
+            }
         }
-        return st.empty();
+        return st.size() == 0;
     }
 };
