9e17032eee17d3f591c0e3df830cf082b5ce41ed
Neerath0804 <139732091+Neerath0804@users.noreply.github.com>
2025-10-06 11:11:22 +0530

    Time: 3 ms (74.83%) | Memory: 15.8 MB (53.87%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftMin(n), rightMin(n), leftMax(n), rightMax(n);
+    long long subArrayRanges(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> leftmin(n), rightmin(n), leftmax(n), rightmax(n);
         stack<int> st;
 
-        // Next & Prev Smaller
+        // --- left min ---
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
-            leftMin[i] = st.empty() ? i + 1 : i - st.top();
-            st.push(i);
-        }
-        while (!st.empty()) st.pop();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
-            rightMin[i] = st.empty() ? n - i : st.top() - i;
+            while (!st.empty() && arr[st.top()] > arr[i]) st.pop();
+            leftmin[i] = st.empty() ? i + 1 : i - st.top();
             st.push(i);
         }
 
-        // Next & Prev Greater
-        while (!st.empty()) st.pop();
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
-            leftMax[i] = st.empty() ? i + 1 : i - st.top();
-            st.push(i);
-        }
-        while (!st.empty()) st.pop();
+        while (!st.empty()) st.pop(); // clear stack
+
+        // --- right min ---
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
-            rightMax[i] = st.empty() ? n - i : st.top() - i;
+            while (!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            rightmin[i] = st.empty() ? n - i : st.top() - i;
             st.push(i);
         }
 
-        long long res = 0;
+        while (!st.empty()) st.pop();
+
+        // --- left max ---
         for (int i = 0; i < n; i++) {
-            long long totalMax = (long long)nums[i] * leftMax[i] * rightMax[i];
-            long long totalMin = (long long)nums[i] * leftMin[i] * rightMin[i];
-            res += (totalMax - totalMin);
+            while (!st.empty() && arr[st.top()] < arr[i]) st.pop();
+            leftmax[i] = st.empty() ? i + 1 : i - st.top();
+            st.push(i);
         }
-        return res;
+
+        while (!st.empty()) st.pop();
+
+        // --- right max ---
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] <= arr[i]) st.pop();
+            rightmax[i] = st.empty() ? n - i : st.top() - i;
+            st.push(i);
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            long long totalMax = (long long)arr[i] * leftmax[i] * rightmax[i];
+            long long totalMin = (long long)arr[i] * leftmin[i] * rightmin[i];
+            ans += (totalMax - totalMin);
+        }
+        return ans;
     }
 };
