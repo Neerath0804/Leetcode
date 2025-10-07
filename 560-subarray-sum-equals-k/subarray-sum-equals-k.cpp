b25691180be62efc91a1657d3e2c83224804df52
Neerath0804 <139732091+Neerath0804@users.noreply.github.com>
2025-10-07 09:20:30 +0530

    Time: 38 ms (79.28%) | Memory: 45.3 MB (67.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         unordered_map<int,int>mpp;
         int sum = 0, count = 0;
-        mpp[0] = 1;
+        mpp[0]=1;
         for(int i = 0; i < n; i++)
         {
             sum += nums[i];
             if(mpp.find(sum - k) != mpp.end())
             {
-                count += mpp[sum - k]; 
+               count +=  mpp[sum - k];
             }
             mpp[sum]++;
         }
-        return count;
+     return count;
     }
 };
